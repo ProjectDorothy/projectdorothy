d405bd1fd42e3899b9bc2ff2b4cb66023ccdc31c
Fotinos Kyriakides <fotinos1997@hotmail.com>
2020-02-08 13:27:57 +0000

    get req capabilities on mock server and working (simple) get req from endpoints

--- endpoints.js
+++ endpoints.js
@@ -1,1 +1,13 @@
+const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest; //only use for local, already loaded on web
+
 const xhttp = new XMLHttpRequest();
+
+const url = "http://localhost:3000";
+
+
+xhttp.open("GET",url,true);
+xhttp.onload = function () {
+    console.log('get req got got!')
+};
+// xhttp.onprogress = function () {console.log('change')};
+xhttp.send();
