4fcd5e01ca2b2e77ce9890ecd86424bdb091769e
Fotinos Kyriakides <fotinos1997@hotmail.com>
2020-02-08 22:36:56 +0000

    Adds player endpoint

--- endpoints.js
+++ endpoints.js
@@ -6,18 +6,8 @@ const xhttp2 = new XMLHttpRequest();
 
 const url = "http://localhost:3000";
 
-/*xhttp.open("GET",url,true);
+xhttp.open("GET",url+'playerA',true);
 xhttp.onload = function () {
     console.log('get req got got!')
 };
-xhttp.send();*/
-
-
-xhttp2.open("PUT",url+'/put',true);
-xhttp2.onload = function () {
-    console.log('put req works')
-
-    let stuff = JSON.parse(this.responseText);
-    console.log(stuff)
-};
-xhttp2.send();
+xhttp.send();
