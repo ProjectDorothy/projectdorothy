ab1353e6162acf648d2d96b6c7d2f9d6780a009d
Fotinos Kyriakides <fotinos1997@hotmail.com>
2020-02-08 13:44:39 +0000

    put req capabilities on server

--- endpoints.js
+++ endpoints.js
@@ -1,6 +1,8 @@
 const XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest; //only use for local, already loaded on web
 
 const xhttp = new XMLHttpRequest();
+const xhttp2 = new XMLHttpRequest();
+
 
 const url = "http://localhost:3000";
 
@@ -11,3 +13,11 @@ xhttp.onload = function () {
 };
 // xhttp.onprogress = function () {console.log('change')};
 xhttp.send();
+
+/*
+xhttp.open("GET",url,true);
+xhttp.onload = function () {
+    console.log('get req got got!')
+};
+// xhttp.onprogress = function () {console.log('change')};
+xhttp.send();*/
--- mock_server.js
+++ mock_server.js
@@ -1,30 +1,19 @@
-// const express = require('express');
-// const api_helper = require('./API_helper');
-// const server = express();
-//
-// const port = 3001;
-//
-// server.use(express.json());
-// server.use((req, res, next) => {
-//     next();
-// });
-//
-//
-//
-// module.exports = server;
-
 const express = require('express');
 const app = express();
 
 const myLogger = function (req, res, next) {
     console.log('LOGGED');
     next()
-}
+};
 
 app.use(myLogger);
 
 app.get('/', function (req, res) {
     res.send('Hello World!')
+});
+
+app.put('/put', (req,res) => {
+    res.send('put req successful')
 });
 
 app.listen(3000);
